f7c2fdf687c9f32cbce9463d50748ac7f48ce55a
最后身边是你就好 <1578333925@qq.com>
2019-12-16 17:26:03 +0800

    添加轮播图

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,1 +1,20 @@
-Page({})
+import { getMultiData } from '../../service/home.js'
+
+Page({
+    data:{
+        banners:[],
+        recommends:[]
+    },
+    onLoad: function(options){
+        //1.请求轮播图以及推荐数据
+        getMultiData().then(res =>{
+            console.log(res)
+            const banners = res.data.data.banner.list;
+            const recommends = res.data.data.recommend.list;
+            this.setData({
+                banners,
+                recommends
+            })
+        })
+    }
+})
